61cf9597234d61af319fbf993a81b82066ba9c13
Sanehagarg <100423126+Sanehagarg@users.noreply.github.com>
2022-07-22 23:45:18 +0530

    Update speakhello.js

--- speakhello.js
+++ speakhello.js
@@ -1,7 +1,19 @@
+// function which will be executed when the first letter of the name does not start with letter j
+
 (function (window) {
+    
+    // variable which will store Hello
     var greeting = "Hello";
+    
+    // function which will be taking name as the argument which do not start with the letter j in the paranthesis 
+    //and will perform the whole functioning
+    //and store the return value in variable speaker
+    
     var speaker = function (name) {
+        
+        //printing statemet
     console.log(greeting + " " + name);
     }
+    
     window.speaker =speaker;
 })(window);
