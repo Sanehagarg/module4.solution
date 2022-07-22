666d457eb21fb6bfa21303cbc527a010911a54d1
Sanehagarg <100423126+Sanehagarg@users.noreply.github.com>
2022-07-22 23:39:32 +0530

    Update speakbye.js

--- speakbye.js
+++ speakbye.js
@@ -1,8 +1,16 @@
+// function which will be executed when the firstLetter of the name is j 
+
 (function (window) {
 
+    // variable to store Good Bye
     var byeGreeting = "Good Bye";
+    
+    //object creation which is equal to a function having name in the paranthesis
     var jSpeaker = function (name) {
+        
+        // printing statement
     console.log(byeGreeting + " " + name);
     }
+    
     window.jSpeaker = jSpeaker;
     })(window);
